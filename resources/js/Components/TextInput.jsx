6f7d31816bb971bee727d1d945253cca33c0772d
Denson <denson753@gmail.com>
2023-03-27 22:19:37 +0800

    feat: slicing login & regis

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,25 +1,61 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
-    const input = ref ? ref : useRef();
+//Proptypes sendiri fungsi sebagai validasi inputan
 
-    useEffect(() => {
-        if (isFocused) {
-            input.current.focus();
-        }
-    }, []);
+TextInput.propTypes = {
+  type: PropTypes.oneOf(['text', 'email', 'password', 'number', 'file']),
+  name: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(['primary', 'error', 'primary-outline']),
+  autoComplete: PropTypes.string,
+  required: PropTypes.bool,
+  isFocused: PropTypes.bool,
+  handleChange: PropTypes.func,
+  placeholder: PropTypes.string,
+  isError: PropTypes.bool,
+}
 
-    return (
-        <div className="flex flex-col items-start">
-            <input
-                {...props}
-                type={type}
-                className={
-                    'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
-                    className
-                }
-                ref={input}
-            />
-        </div>
-    );
-});
+export default function TextInput({
+  type = 'text',
+  name,
+  value,
+  defaultValue,
+  className = '',
+  variant = 'primary',
+  autoComplete,
+  isFocused,
+  handleChange,
+  placeholder,
+  isError,
+  required,
+}) {
+  const inputRef = useRef()
+
+  useEffect(() => {
+    if (isFocused) {
+      inputRef.current.focus()
+    }
+  }, [])
+
+  return (
+    <div className="flex flex-col items-start">
+      <input
+        type={type}
+        name={name}
+        value={value}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        className={`rounded-2xl bg-form-bg py-[13px] px-7 w-full ${
+          isError && 'input-error'
+        } input-${variant} ${className}`}
+        autoComplete={autoComplete}
+        required={required}
+        onChange={handleChange}
+        placeholder={placeholder}
+      />
+    </div>
+  )
+}
